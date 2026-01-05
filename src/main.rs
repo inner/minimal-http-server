da760ca8a389dcf6efa71963029cbfaf8e912bc7
Simonas <simonas.pacauskas@gmail.com>
2026-01-05 19:25:05 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use std::net::TcpListener;
+use std::io::{Error, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(s) => {
+                thread::spawn(move || {
+                    handle_connection(&s).unwrap();
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: &TcpStream) -> Result<(), Error> {
+    stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+    Ok(())
 }
