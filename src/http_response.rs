f4f9cdeaadeca7228029db5fc6c213e84d80b5f4
Simonas <simonas.pacauskas@gmail.com>
2026-02-04 18:21:52 +0200

    codecrafters submit [skip ci]

--- src/http_response.rs
+++ src/http_response.rs
@@ -1,2 +1,26 @@
-#[warn(unused)]
-pub struct HttpResponse;
+use std::collections::HashMap;
+
+pub struct HttpResponse {
+    pub http_line: String,
+    pub body: String,
+    pub headers: HashMap<String, String>,
+}
+
+impl HttpResponse {
+    pub fn as_bytes(&self) -> Vec<u8> {
+        let mut response = Vec::new();
+        response.extend_from_slice(format!("{}\r\n", self.http_line).as_bytes());
+
+        for (k, v) in &self.headers {
+            response.extend_from_slice(format!("{k}: {v}\r\n").as_bytes());
+        }
+
+        response.extend_from_slice("\r\n".as_bytes());
+
+        if !self.body.is_empty() {
+            response.extend_from_slice(self.body.as_bytes());
+        }
+
+        response
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ mod http_request;
 mod http_response;
 
 use crate::http_request::HttpRequest;
+use std::collections::HashMap;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+
+use self::http_response::HttpResponse;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -27,12 +30,38 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let request = HttpRequest::new(&stream)?;
 
     let status_line = if request.path == "/" {
-        "HTTP/1.1 200 OK\r\n\r\n"
+        let response = HttpResponse {
+            http_line: String::from("HTTP/1.1 200 OK"),
+            body: String::new(),
+            headers: HashMap::new(),
+        };
+        response.as_bytes()
+    } else if let Some(echo) = request.path.strip_prefix("/echo/") {
+        let mut headers = HashMap::new();
+
+        headers.insert(
+            String::from("Content-Length"),
+            String::from(echo.len().to_string()),
+        );
+
+        headers.insert(String::from("Content-Type"), String::from("text/plain"));
+
+        let response = HttpResponse {
+            http_line: String::from("HTTP/1.1 200 OK"),
+            headers,
+            body: String::from(echo),
+        };
+        response.as_bytes()
     } else {
-        "HTTP/1.1 404 Not Found\r\n\r\n"
+        let response = HttpResponse {
+            http_line: String::from("HTTP/1.1 404 Not Found"),
+            body: String::new(),
+            headers: HashMap::new(),
+        };
+        response.as_bytes()
     };
 
-    stream.write_all(status_line.as_bytes())?;
+    stream.write_all(&status_line)?;
 
     Ok(())
 }
