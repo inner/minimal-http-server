7caaa1c493501f11f8a5de0ff53cf9dc20333835
inner <simonas.pacauskas@gmail.com>
2026-02-20 18:38:35 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ mod http;
 mod http_request;
 mod http_response;
 mod router;
+mod thread_pool;
 
 use self::http_request::HttpRequest;
 use self::router::Router;
+use self::thread_pool::ThreadPool;
 
 use std::collections::HashMap;
+use std::env;
+use std::error::Error;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
-use std::{env, thread};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
     let args: Vec<String> = env::args().skip(1).collect();
     let map: Arc<HashMap<String, String>> = Arc::new(
         args.chunks(2)
@@ -27,25 +30,31 @@ fn main() {
             .collect(),
     );
 
+    let pool = ThreadPool::build(10)?;
+
     for stream in listener.incoming() {
         match stream {
             Ok(s) => {
                 let map = Arc::clone(&map);
-                thread::spawn(move || {
-                    handle_connection(map, s).unwrap();
-                });
+                pool.execute(|| {
+                    if let Err(e) = handle_connection(map, s) {
+                        eprintln!("connection error: {e}");
+                    }
+                })?;
             }
             Err(e) => {
                 eprintln!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
 
 fn handle_connection(
     args: Arc<HashMap<String, String>>,
     mut stream: TcpStream,
-) -> std::io::Result<()> {
+) -> Result<(), Box<dyn Error>> {
     let request = HttpRequest::new(&stream)?;
     let response = Router::handle(&request, &args);
     stream.write_all(&response.as_bytes())?;
---
+++ src/thread_pool.rs
@@ -0,0 +1,102 @@
+use std::error::Error;
+use std::sync::{Arc, Mutex, mpsc, mpsc::Receiver};
+use std::thread;
+use std::thread::JoinHandle;
+
+pub struct Worker {
+    id: usize,
+    thread: Option<JoinHandle<()>>,
+}
+
+type ReceiverEnd = Arc<Mutex<Receiver<Job>>>;
+
+impl Worker {
+    pub fn new(id: usize, rx: ReceiverEnd) -> Result<Self, Box<dyn Error>> {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = {
+                    let rx = rx.lock().unwrap_or_else(|poisoned| poisoned.into_inner());
+                    rx.recv()
+                };
+
+                match job {
+                    Ok(job) => {
+                        println!("Worker {} got a job; executing.", id);
+                        job();
+                    }
+                    Err(_) => {
+                        println!("Worker {} shutting down.", id);
+                        break;
+                    }
+                }
+            }
+        });
+
+        Ok(Worker {
+            id,
+            thread: Some(thread),
+        })
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    pub workers: Vec<Worker>,
+    tx: Option<mpsc::Sender<Job>>,
+}
+
+impl ThreadPool {
+    fn new(size: usize) -> Result<Self, Box<dyn Error>> {
+        let (tx, rx) = mpsc::channel();
+        let rx = Arc::new(Mutex::new(rx));
+
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&rx))?);
+        }
+
+        Ok(ThreadPool {
+            workers,
+            tx: Some(tx),
+        })
+    }
+
+    pub fn build(size: usize) -> Result<Self, Box<dyn Error>> {
+        if size > 0 {
+            Ok(ThreadPool::new(size)?)
+        } else {
+            Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "size must be greater than 0",
+            )))
+        }
+    }
+
+    pub fn execute<F>(&self, f: F) -> Result<(), Box<dyn Error>>
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.tx
+            .as_ref()
+            .ok_or("thread pool has been shut down")?
+            .send(job)
+            .map_err(|e| e.into())
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.tx.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
