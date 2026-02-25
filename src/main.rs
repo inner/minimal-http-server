7724ad878354742ad9d055de2d8af57f74ceba90
inner <simonas.pacauskas@gmail.com>
2026-02-25 09:39:02 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ fn handle_return_file(req: &HttpRequest, args: &HashMap<String, String>) -> Http
 fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:4221")?;
     let args: Vec<String> = env::args().skip(1).collect();
-    let map: Arc<HashMap<String, String>> = Arc::new(
+    let map: &'static HashMap<String, String> = Box::leak(Box::new(
         args.chunks(2)
             .filter_map(|chunk| {
                 if chunk.len() == 2 && chunk[0].starts_with("--") {
@@ -90,7 +90,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             })
             .collect(),
-    );
+    ));
 
     let router = Router::new()
         .add(Method::Get, "/", handle_root)
@@ -104,7 +104,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     for stream in listener.incoming() {
         match stream {
             Ok(s) => {
-                let map = Arc::clone(&map);
                 let router = Arc::clone(&router);
                 pool.execute(|| {
                     if let Err(e) = handle_connection(map, router, s) {
@@ -122,7 +121,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn handle_connection(
-    args: Arc<HashMap<String, String>>,
+    args: &HashMap<String, String>,
     router: Arc<Router>,
     mut stream: TcpStream,
 ) -> Result<(), Box<dyn Error>> {
