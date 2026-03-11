d5cde91f8705f09b06e81b87c23c9cb3ebe9404c
inner <simonas.pacauskas@gmail.com>
2026-03-11 20:30:22 +0200

    codecrafters submit [skip ci]

--- src/files.rs
+++ src/files.rs
@@ -1,11 +1,15 @@
-use std::fs::{File, create_dir};
-use std::io::{Error, Read, Write};
+use std::fs::{self, File, create_dir};
+use std::io::{Error, ErrorKind, Read, Write};
 use std::path::Path;
 
 pub struct FileManager;
 
 impl FileManager {
     pub fn create(path: &Path, file_name: &str, body: &[u8]) -> Result<(), Error> {
+        if file_name.split('/').any(|s| s == "..") {
+            return Err(Error::new(ErrorKind::PermissionDenied, "invalid path"));
+        }
+
         if !path.exists() {
             create_dir(path)?;
         }
@@ -16,7 +20,14 @@ impl FileManager {
     }
 
     pub fn read(path: &Path, file_name: &str) -> Result<Vec<u8>, Error> {
-        let mut file = File::open(path.join(file_name))?;
+        let full_path = path.join(file_name);
+        let canonical = fs::canonicalize(&full_path)?;
+
+        if !canonical.starts_with(path) {
+            return Err(Error::new(ErrorKind::PermissionDenied, "invalid path"));
+        }
+
+        let mut file = File::open(canonical)?;
         let mut contents: Vec<u8> = Vec::new();
         file.read_to_end(&mut contents)?;
         Ok(contents)
