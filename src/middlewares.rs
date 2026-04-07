358e2a277b0360832155479a7cfa6efb6b8483d1
inner <simonas.pacauskas@gmail.com>
2026-04-07 16:58:30 +0300

    codecrafters submit [skip ci]

--- src/middlewares.rs
+++ src/middlewares.rs
@@ -6,12 +6,10 @@ pub struct Middlewares;
 impl Middlewares {
     pub fn run<'a>(req: &'a HttpRequest, res: &'a mut HttpResponse) {
         let Some(content_encoding) = req.headers.get("accept-encoding") else {
-            println!("test1");
             return;
         };
 
         let Ok(_) = res.with_encoding(String::from(content_encoding)) else {
-            println!("test2");
             return;
         };
     }
